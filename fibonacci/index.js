92de2ed42a8f4fea16033aa74d04d4e279d2cf2b
Michal Drzazga <mdrzazga@microstrategy.com>
2019-04-30 22:00:51 +0200

    fib() and test cases

--- fibonacci/index.js
+++ fibonacci/index.js
@@ -1,8 +1,25 @@
-const fibonnacciNumber = (num) => {
-    if(num<2){
-        return num;
+export const fib = (num) => {
+  const res = [];
+  if (isNaN(num)) {
+    throw new Error('Parameter `num` must be a number of a number-like string');
+  }
+  const num1 = Number.parseInt(num);
+  if (num1 != num || num1 != parseFloat(num)) {
+    throw new Error('Parameter `num` must be an integer');
+  }
+  if (num1 < 1) {
+    throw new Error('Parameter `num` must be greater than 0');
+  } else {
+    for (let i = 0; i < num; i++) {
+      if (i < 2) {
+        res.push(i);
+      } else {
+        const a = Array.from(res);
+        const v1 = a.pop();
+        const v2 = a.pop();
+        res.push(v1 + v2);
+      }
     }
-    else {
-        return fibonnacciNumber(num-1) + fibonnacciNumber(num-2);
-    }
-}
+    return res;
+  }
+};
